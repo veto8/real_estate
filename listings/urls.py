5b5f506ac169c8886de42ff4496ae29792eff6fa
codewiz <c.chaiwat.ch@gmail.com>
2021-02-28 12:36:59 +0700

    update: listing mockup to lising from data source
    Signed-off-by: codewiz <c.chaiwat.ch@gmail.com>

--- listings/urls.py
+++ listings/urls.py
@@ -6,6 +6,6 @@ from . import views
 # TODO Add route for listings
 urlpatterns = [
     path('', views.index, name='listings'),
-    path('<int:listing_id', views.listing, name='listing'),
+    path('<int:listing_id>', views.listing, name='listing'),
     path('search', views.search, name='search'),
 ]
