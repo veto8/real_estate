6f20dd324442ea9d406e5e0ee18133ef28a5968d
codewiz <c.chaiwat.ch@gmail.com>
2021-03-13 22:19:50 +0700

    update: about page

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 
 def index(request):
@@ -11,6 +12,14 @@ def index(request):
     }
     return render(request, 'pages/index.html', context)
 
-def about(request):
-    return render(request, 'pages/about.html')
 
+def about(request):
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
