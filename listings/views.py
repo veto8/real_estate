199c08d21ce96ea42166234ea08a03e6b11ef256
codewiz <c.chaiwat.ch@gmail.com>
2021-03-14 13:35:09 +0700

    add: choice, update: search index

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.paginator import Paginator
 from .models import Listing
+from .choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
@@ -24,4 +25,9 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+    }
+    return render(request, 'listings/search.html', context)
