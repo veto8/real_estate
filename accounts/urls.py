df67e209cad34732bfa79488bbf2411decd8799b
codewiz <c.chaiwat.ch@gmail.com>
2021-03-21 11:29:26 +0700

    add: dash board page
    Signed-off-by: codewiz <c.chaiwat.ch@gmail.com>

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('register', views.register, name='register'),
-    path('dash_board', views.register, name='dash_board'),
+    path('dash_board', views.dash_board, name='dash_board'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,13 +5,26 @@ from django.contrib.auth.models import User
 
 def login(request):
     if request.method == 'POST':
-        pass
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dash_board')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index')
 
 
 def register(request):
